f780d5758e6b8cd6be05c4bdc304c4ab18edf369
Johannes Rust <johannes.rust@dfki.de>
2024-08-19 17:01:23 +0200

    feat: Implement remaining expected gradients. Needs testing.

--- sw/ai_accelerator_binCof_NN/src/nn/expected_gradients.cpp
+++ sw/ai_accelerator_binCof_NN/src/nn/expected_gradients.cpp
@@ -1,9 +1,11 @@
+#include <random>
+
 #include "expected_gradients.hpp"
 #include "tensor.hpp"
 #include "module.hpp"
 
 template <typename T>
-Tensor<T> expected_gradients(nn::Module<T> &module, Tensor<T> &input, Tensor<T> &background_dataset) {
+Tensor<T> expected_gradients(nn::Module<T> &module, Tensor<T> &input, Tensor<T> &background_dataset, int n_samples) {
     // Create uniform distribution generator
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -12,15 +14,21 @@ Tensor<T> expected_gradients(nn::Module<T> &module, Tensor<T> &input, Tensor<T> 
     std::uniform_int_distribution<int> index_dist(0, background_dataset.size() - 1);
     // Create a tensor to store the expected gradients
     Tensor<T> grads = Tensor<T>::zeros(input.getShape());
+    Tensor<T> input_pred = module.forward(input);
+
     // Use a form of Monte Carlo estimates to calculate the expected gradients
     // E[f(x)] = 1/N * sum(f(x_i))
     // See https://sourav-64777.medium.com/estimating-expectations-and-gibbs-sampling-3c9e1b7e6c20
-    for (int i = 0; i < 200; i++) {
-        // Sample from alpha
-        alpha = alpha_dist(gen);
-
+    for (int i = 0; i < n_samples; i++) {
+        float alpha = alpha_dist(gen);
         // Select a random sample from the background dataset
         int random_index = index_dist(gen);
         Tensor<T> random_sample = background_dataset[random_index];
+        Tensor<T> input_minus_random = input - random_sample;
+        Tensor<T> pred_current = module.forward(random_sample + (input_minus_random * alpha));
+        Tensor<T> current_grad = module.backward(pred_current - input_pred);
+        grads += input_minus_random.mul(current_grad);
     }
+    grads /= n_samples;
+    return grads;
 }
