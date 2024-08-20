0eb230730c4d6625e5a20431337f25a82988adc5
Johannes Rust <johannes.rust@dfki.de>
2024-08-20 20:14:21 +0200

    feat: adapt the exapt explainer code from shap.cpp to Tensor class

--- sw/ai_accelerator_binCof_NN/src/nn/expected_gradients.cpp
+++ sw/ai_accelerator_binCof_NN/src/nn/expected_gradients.cpp
@@ -32,3 +32,113 @@ Tensor<T> expected_gradients(nn::Module<T> &module, Tensor<T> &input, Tensor<T> 
     grads /= n_samples;
     return grads;
 }
+
+////////////////////////////////////////////////////////
+// The new stuff adapted from shap.cpp
+////////////////////////////////////////////////////////
+
+
+template <typename T>
+void replaceValues(Tensor<T> array, const std::vector<bool>& mask, const std::vector<float>& newValues) {
+	// Replace values in the array based on the mask
+    std::vector<T> data = array.getData();
+	for (std::size_t i = 0; i < data.size(); ++i) {
+		if (!mask[i]) {
+			data[i] = newValues[i];
+		}
+	}
+    array.setData(data);
+}
+
+uint32_t binomialCoeff(uint32_t n, uint32_t k) {
+	// Calculate the binomial coefficient
+/** if (k > n - k) {
+		k = n - k;
+	}
+	uint32_t res = 1;
+	for (uint32_t i = 0; i < k; ++i) {  // tested
+		res *= (n - i);
+		res /= (i + 1);
+	}
+	return res;
+*/
+	return make_syscall(n, k, 701);
+}
+
+uint64_t factorial(uint64_t n) {
+	// Factorials larger than 20 cause overflow
+	if (n > 20) return -1;
+
+	uint64_t res = 1;
+	for (uint32_t i = 1; i <= n; ++i) {
+		res *= i;
+	}
+	return res;
+}
+
+template <typename T>
+Tensor<T> sampleFromData(const Tensor<T> data) {
+	std::vector<float> res;
+	for (size_t i=0; i < data.getShape()[0]; ++i) {
+		res.push_back(data[rand() % data.size()][i]);
+	}
+	Tensor<T> res_tensor = Tensor<T>(res);
+    return res_tensor;
+}
+
+float shapleyFrequency(uint32_t n, uint32_t s) {
+	// Calculate the frequency of feature i in the shapley value calculation.
+	// Equvalent to (n-s-1)! * s! / n!, but without overflow
+	if (n-s <= 0) {
+		return 0;
+	}
+	return 1.0f / (binomialCoeff(n, s) * (n-s));
+}
+
+template <typename T>
+Tensor<T> explainPrediction(Tensor<T> input_data, float (func)(const float*, unsigned int)) {
+	Tensor<T> bgData = Tensor<T>::zeros(input_data.getShape());
+	return explainPrediction(input_data, func, bgData);
+}
+
+template <typename T>
+Tensor<T> explainPrediction(Tensor<T> input_data, float (func)(const float*, unsigned int), Tensor<T> background_data) {
+
+	uint32_t n = input_data.size();
+	Tensor<T> shapley_values = Tensor<T>::zeros(input_data.getShape());
+	std::vector<bool> mask(n, false);
+
+	for (uint32_t i = 0; i < n; ++i) {  // Shapley value for feature i
+		uint64_t num_subsets = 1 << (n - 1);  // 2^(n-1) subsets of N\{i}
+		for (uint32_t j = 0; j < num_subsets; ++j) {
+			auto mask = getAsBoolVector(j, n - 1);
+			mask.insert(mask.begin() + i, false);  // Add the feature back in
+			// We need to know |S|, the size of the subset
+			uint32_t subsetSize = 0;
+			for (uint32_t k = 0; k < n; ++k) {
+				if (mask[k]) {
+					subsetSize++;
+				}
+			}
+			Tensor<T> data_masked(input_data);
+			auto sampled_background = sampleFromData(background_data);
+			replaceValues(data_masked, mask, sampled_background);
+			auto pred_without_i = func(&data_masked[0], data_masked.size());
+			
+			data_masked[i] = input_data[i];
+			auto pred_with_i = func(&data_masked[0], data_masked.size());
+			shapley_values[i] += shapleyFrequency(n, subsetSize) * (pred_with_i - pred_without_i);
+		}
+	}
+	return shapley_values;
+}
+
+std::vector<bool> getAsBoolVector(uint32_t n, uint16_t length) {
+	std::vector<bool> res(length, false);
+	for (uint16_t i = 0; i < length; ++i) {
+		if (n & (1 << i)) {
+			res[i] = true;
+		}
+	}
+	return res;
+}
