9ef2fab73a00ea383d726ddb7f91cb9d973c2b45
Johannes Rust <johannes.rust@dfki.de>
2024-09-09 15:02:35 +0200

    refactor: Add convenience function for backward pass in nn::Module
    This commit adds a convenience function to the nn::Module class that allows the user to pass a scalar value for the backward pass. This function simplifies the code and improves readability.

--- sw/ai_accelerator_binCof_NN/src/nn/module.hpp
+++ sw/ai_accelerator_binCof_NN/src/nn/module.hpp
@@ -41,6 +41,16 @@ namespace nn
         virtual Tensor<T> backward(const Tensor<T>& gradOutput) = 0;
 
         /**
+         * @brief Performs the backward pass of the module.
+         * 
+         * This function is a convenience function that allows the user to pass a scalar value.
+         * 
+         * @param gradOutput The gradient value from the next module in the network.
+         * @return The gradient tensor obtained after applying the backward pass.
+         */
+        Tensor<T> backward(const float &gradOutput) {return backward(Tensor<T>(gradOutput));}
+
+        /**
          * @brief Updates the parameters of the module using the specified learning rate.
          * 
          * This method updates the parameters of the module using the specified learning rate.
