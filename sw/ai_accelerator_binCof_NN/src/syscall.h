b727e9c905aa92c703b3775d070096b8409c0a41
Johannes Rust <johannes.rust@dfki.de>
2024-09-17 16:55:13 +0200

    refactor: Delete all versions of the ai_accelerator and only leave one.

--- sw/ai_accelerator_binCof_NN/src/syscall.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-extern "C" int make_syscall(int arg1, int arg2, int syscall_number);
-
-#define START_TRACE make_syscall(1, 0, 700)
-#define STOP_TRACE make_syscall(0, 0, 700)
--- sw/ai_accelerator_binCof_syscall/src/syscall.h
+++
@@ -1,1 +0,0 @@
-extern "C" int make_syscall(int arg1, int arg2, int syscall_number);
