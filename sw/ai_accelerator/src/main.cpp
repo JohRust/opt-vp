8f25ce7fc575c3c54b69694b0146f4ea16d0f2c4
Johannes <johannes.rust@web.de>
2025-05-08 11:50:23 +0200

    Add Macro to quickly switch between the two shapley methods

--- sw/ai_accelerator/src/main.cpp
+++ sw/ai_accelerator/src/main.cpp
@@ -16,6 +16,9 @@ extern "C" {
 // Otherwise linking fails.
 void *__dso_handle = 0;
 
+#define EXACT_SHAP 1
+#define EXPECTED_GRAD 0
+
 int main(int argc, char **argv) {
 	init_dma();
 	// Build a sequential model
@@ -34,14 +37,19 @@ int main(int argc, char **argv) {
 	START_TRACE;
 	Tensor<float> input_data({1.0, 2.0, 3.0, 4.0}, {1, 4});
 	Tensor<float> background_data({0.0,0.0,0.0,0.0}, {1, 4});
-	//Tensor<float> shapley_values = expected_gradients<float>(model, input_data, background_data, 50);
+	#if EXPECTED_GRAD
+	Tensor<float> shapley_values = expected_gradients<float>(model, input_data, background_data, 50);
+	STOP_TRACE;
+	printf("Shapley values:\n");
+	printf("%s\n", shapley_values.toString().c_str());
+	#endif
+	#if EXACT_SHAP
 	Tensor<float> shapley_values_exact = exact_shap<float>(model, input_data, background_data);
 	STOP_TRACE;
-	
-	//printf("Shapley values:\n");
-	//printf("%s\n", shapley_values.toString().c_str());
 	printf("Exact Shapley values:\n");
 	printf("%s\n", shapley_values_exact.toString().c_str());
+	#endif
+
 	Tensor<float> preds = model.forward(input_data);
 	printf("Predictions: %s\n", preds.toString().c_str());
 	float expected_value = model.forward(background_data).mean();
