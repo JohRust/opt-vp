24c92dc407d1ea93aacde17170ed929697c0be1f
Johannes Rust <johannes.rust@dfki.de>
2026-01-14 17:36:36 +0100

    Move counting traced instructions

--- vp/src/core/rv32/iss.cpp
+++ vp/src/core/rv32/iss.cpp
@@ -167,7 +167,6 @@ void ISS::exec_step() {
 	uint64_t cycles_diff = _compute_and_get_current_cycles() - prev_cycles;
 
 	if (record_traces) {
-		traced_num_instr++;
 //Instead of updateing the entry at the start of the loop with data from the last iteration
 //move this part to the end as memory accesses have to be calculated first
 //otherwise memory accesses are off by 1
@@ -2014,6 +2013,9 @@ void ISS::switch_to_trap_handler(PrivilegeLevel target_mode) {
 
 void ISS::performance_and_sync_update(Opcode::Mapping executed_op) {
     ++total_num_instr;
+	if (record_traces) {
+		++traced_num_instr;
+	}
 
 	if (!csrs.mcountinhibit.fields.IR)
 		++csrs.instret.reg;
@@ -2842,7 +2844,7 @@ void ISS::show() {
 		}
 	}
 	std::cout << "total instructions: " << csrs.instret.reg << "(" << total_num_instr << ")" << " [" << total_percent*100 << "]" << std::endl;
-	std::cout << "traced instructions: " << total_num_instr << std::endl;
+	std::cout << "traced instructions: " << traced_num_instr << std::endl;
 	std::cout << "total cycles: " << _compute_and_get_current_cycles() << std::endl;
 
 	if(discovered_sequences.empty()){
